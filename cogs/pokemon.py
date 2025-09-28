f6d79236cde0de9ba027a43906de88e7bcd1d7ac
Ueslei Paim <uesleibros@gmail.com>
2025-09-28 00:53:15 -0300

    Update pokemon.py

--- cogs/pokemon.py
+++ cogs/pokemon.py
@@ -79,7 +79,7 @@ class Pokemon(commands.Cog):
 			desc_lines.append(
 				f"`{format_poke_id(poke_id)}`　{emoji}{shiny} **{nickname}** {gender} {status}　•　Lv. {level}　•　{iv_percent_}%"
 			)
-			await pm.service.close()
+			await pm.service.client.close()
 			del poke
 
 		chunk_size = 20
@@ -103,6 +103,7 @@ class Pokemon(commands.Cog):
 
 async def setup(bot: commands.Bot):
 	await bot.add_cog(Pokemon(bot))
+
 
 
 
