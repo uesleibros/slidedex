af81640f3ee128ab732e6c21d4f5c963bd93cf85
Ueslei Paim <uesleibros@gmail.com>
2025-09-29 23:07:57 -0300

    Update constants.py

--- pokemon_sdk/constants.py
+++ pokemon_sdk/constants.py
@@ -37,6 +37,37 @@ REGIONS_GENERATION = {
 	"generation-ix": "Paldea"
 }
 
+TYPE_CHART = {
+	"normal":   {"super": set(),                              "not": {"rock","steel"},                "immune": {"ghost"}},
+	"fire":     {"super": {"grass","ice","bug","steel"},      "not": {"fire","water","rock","dragon"},"immune": set()},
+	"water":    {"super": {"fire","ground","rock"},           "not": {"water","grass","dragon"},      "immune": set()},
+	"grass":    {"super": {"water","ground","rock"},          "not": {"fire","grass","poison","flying","bug","dragon","steel"}, "immune": set()},
+	"electric": {"super": {"water","flying"},                 "not": {"electric","grass","dragon"},   "immune": {"ground"}},
+	"ice":      {"super": {"grass","ground","flying","dragon"},"not": {"fire","water","ice","steel"},"immune": set()},
+	"fighting": {"super": {"normal","ice","rock","dark","steel"}, "not": {"poison","flying","psychic","bug","fairy"}, "immune": {"ghost"}},
+	"poison":   {"super": {"grass","fairy"},                  "not": {"poison","ground","rock","ghost"}, "immune": {"steel"}},
+	"ground":   {"super": {"fire","electric","poison","rock","steel"}, "not": {"grass","bug"}, "immune": {"flying"}},
+	"flying":   {"super": {"grass","fighting","bug"},         "not": {"electric","rock","steel"},     "immune": set()},
+	"psychic":  {"super": {"fighting","poison"},              "not": {"psychic","steel"},             "immune": {"dark"}},
+	"bug":      {"super": {"grass","psychic","dark"},         "not": {"fire","fighting","poison","flying","ghost","steel","fairy"}, "immune": set()},
+	"rock":     {"super": {"fire","ice","flying","bug"},      "not": {"fighting","ground","steel"},   "immune": set()},
+	"ghost":    {"super": {"psychic","ghost"},                "not": {"dark"},                        "immune": {"normal"}},
+	"dragon":   {"super": {"dragon"},                         "not": {"steel"},                       "immune": {"fairy"}},
+	"dark":     {"super": {"psychic","ghost"},                "not": {"fighting","dark","fairy"},     "immune": set()},
+	"steel":    {"super": {"ice","rock","fairy"},             "not": {"fire","water","electric","steel"}, "immune": set()},
+	"fairy":    {"super": {"fighting","dragon","dark"},       "not": {"fire","poison","steel"},       "immune": set()},
+}
+
+STAT_ALIASES = {
+	"hp": ["hp"],
+	"atk": ["atk","attack"],
+	"def": ["def","defense"],
+	"sp_atk": ["sp_atk","spa","special-attack","spatk","sp_att","spatt"],
+	"sp_def": ["sp_def","spd","special-defense","spdef","sp_defense"],
+	"speed": ["speed","spe"]
+}
+
 VERSION_GROUPS = ("firered-leafgreen", "emerald", "ruby-sapphire")
 STAT_KEYS = ("hp", "attack", "defense", "special-attack", "special-defense", "speed")
+
 SHINY_ROLL = 8192
