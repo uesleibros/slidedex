8ff8d8a9a750f83a9b748968b9439fd8e9a45359
Ueslei Paim <uesleibros@gmail.com>
2025-09-30 23:36:16 -0300

    Update canvas.py

--- utils/canvas.py
+++ utils/canvas.py
@@ -33,7 +33,8 @@ def _compose_pokemon(
 ) -> io.BytesIO:
 	composed = background.copy()
 	try:
-		spr = _process_sprite_crop(sprite_bytes, box_size, box_size, crop=False)
+		sprite_box_size = int(box_size * 0.8)
+		spr = _process_sprite_crop(sprite_bytes, sorite_box_size, sprite_box_size, crop=False)
 		x = (composed.width - spr.width) // 2
 		y = ground_y - spr.height
 		composed.paste(spr, (x, y), spr)
@@ -123,3 +124,4 @@ async def compose_battle_async(*args, **kwargs) -> io.BytesIO:
 
 async def compose_profile_async(*args, **kwargs) -> io.BytesIO:
 	return await asyncio.to_thread(_compose_profile, *args, **kwargs)
+
