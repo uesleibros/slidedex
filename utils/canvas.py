083cf04ea63bbc6016563436442f91f08634540b
Ueslei Paim <uesleibros@gmail.com>
2025-10-01 00:09:57 -0300

    Update canvas.py

--- utils/canvas.py
+++ utils/canvas.py
@@ -92,7 +92,7 @@ def _compose_battle(
 	try:
 		if player_bytes:
 			player_box_size = int(box_size * 1.2)
-			p = _process_sprite_crop(player_bytes, player_box_size, player_box_size, crop=False, scale_boost=1.3, max_height_ratio=0.95)
+			p = _process_sprite_crop(player_bytes, player_box_size, player_box_size, crop=False, scale_boost=1.15, max_height_ratio=0.95)
 			composed.paste(p, (player_x, player_ground_y - p.height), p)
 		if enemy_bytes:
 			enemy_box_size = int(box_size * 0.6)
@@ -135,3 +135,4 @@ async def compose_battle_async(*args, **kwargs) -> io.BytesIO:
 async def compose_profile_async(*args, **kwargs) -> io.BytesIO:
 	return await asyncio.to_thread(_compose_profile, *args, **kwargs)
 	
+
