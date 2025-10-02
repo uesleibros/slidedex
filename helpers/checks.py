1d416dd1174d238496ac30767160a05495d77920
Ueslei Paim <uesleibros@gmail.com>
2025-10-01 22:40:13 -0300

    Update checks.py

--- helpers/checks.py
+++ helpers/checks.py
@@ -1,6 +1,35 @@
 import discord
+from __main__ import battle_tracker
 from functools import wraps
 from discord.ext import commands
+
+def not_in_battle():
+	def decorator(func):
+		@wraps(func)
+		async def wrapper(self, ctx: commands.Context, *args, **kwargs):
+			user_id = str(ctx.author.id)
+			
+			if battle_tracker.is_battling(user_id):
+				await ctx.send("Você não pode usar este comando durante uma batalha!")
+				return
+			
+			return await func(self, ctx, *args, **kwargs)
+		return wrapper
+	return decorator
+
+def requires_battle():
+	def decorator(func):
+		@wraps(func)
+		async def wrapper(self, ctx: commands.Context, *args, **kwargs):
+			user_id = str(ctx.author.id)
+			
+			if not battle_tracker.is_battling(user_id):
+				await ctx.send("Você precisa estar em batalha para usar este comando!")
+				return
+			
+			return await func(self, ctx, *args, **kwargs)
+		return wrapper
+	return decorator
 
 def requires_account():
 	def decorator(func):
@@ -17,4 +46,5 @@ def requires_account():
 			
 			return await func(self, ctx, *args, **kwargs)
 		return wrapper
+
 	return decorator
