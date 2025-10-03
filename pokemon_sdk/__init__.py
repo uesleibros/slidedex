561a20aa798449119a5708282e06b4d891abe872
Ueslei Paim <uesleibros@gmail.com>
2025-10-03 11:29:05 -0300

    Update __init__.py

--- pokemon_sdk/__init__.py
+++ pokemon_sdk/__init__.py
@@ -1,5 +1,5 @@
 from .manager import PokemonManager
-from .repository import PokemonRepository
 from .services import PokeAPIService
 from .calculations import calculate_stats, generate_stats, generate_pokemon_data, iv_total, iv_percent
+
 from .constants import NATURES, VERSION_GROUPS, SHINY_ROLL
