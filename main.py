d29fd141e33f58b7219c086f5af2e3c62dbc5e30
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2025-10-03 22:28:22 -0300

    update: things.

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def on_message(message: discord.Message):
 	
 	if bot.user.mentioned_in(message) and not message.mention_everyone:
 		if message.content.strip() in [f'<@{bot.user.id}>', f'<@!{bot.user.id}>']:
-			await message.channel.send(f"Olá <{message.author.mention}>! Meu prefixo é `{bot.command_prefix}`\nUse `{bot.command_prefix}help` para ver todos os comandos!")
+			await message.channel.send(f"Olá {message.author.mention}! Meu prefixo é `{bot.command_prefix}`\nUse `{bot.command_prefix}help` para ver todos os comandos!")
 	
 	await bot.process_commands(message)
 
