7e1e5b1b775052ff3b5b83efda381a074eb1f7ab
Ueslei Paim <uesleibros@gmail.com>
2025-10-03 23:09:58 -0300

    Delete pokemon_sdk/__init__.py

--- pokemon_sdk/__init__.py
+++
@@ -1,6 +0,0 @@
-from .manager import PokemonManager
-from .services import PokeAPIService
-from .calculations import calculate_stats, generate_stats, generate_pokemon_data, iv_total, iv_percent, calculate_max_hp, adjust_hp_on_level_up
-
-from .constants import NATURES, VERSION_GROUPS, SHINY_ROLL, STAT_KEYS, STAT_ALIASES, TYPE_CHART, REGIONS_GENERATION
-
