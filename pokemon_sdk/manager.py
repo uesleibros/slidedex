359f6527f1ae49234a382d944f9e5b4b6fb8ff08
Ueslei Paim <uesleibros@gmail.com>
2025-10-04 01:27:26 -0300

    Update manager.py

--- pokemon_sdk/manager.py
+++ pokemon_sdk/manager.py
@@ -680,6 +680,10 @@ class PokemonManager:
 	    if levels_gained:
 	        move_result = await self.process_level_up(owner_id, pokemon_id, levels_gained, notify_message)
 	        result["move_learning"] = move_result
+			
+			if notify_message:
+				pokemon = self.tk.get_pokemon(owner_id, pokemon_id)
+				await self._send_level_up_notification(notify_message, result, move_result)
 	    else:
 	        result["move_learning"] = {
 	            "learned": [],
@@ -747,3 +751,4 @@ class PokemonManager:
 
 	async def close(self):
 		await self.service.close()
+
