16219d17ffe2d412dc8d487c702b32642d514ee7
Ueslei Paim <uesleibros@gmail.com>
2025-10-06 14:19:00 -0300

    Update constants.py

--- cogs/bag/constants.py
+++ cogs/bag/constants.py
@@ -8,11 +8,6 @@ CATEGORY_NAMES = {
 	"key_items": "Itens Chave"
 }
 
-EVOLUTION_STONES = [
-	"water-stone", "fire-stone", "thunder-stone", 
-	"leaf-stone", "moon-stone", "sun-stone"
-]
-
 ITEM_EMOJIS = {
 	# Poké Balls
 	"poke-ball": PokeBallSystem.get_ball_emoji("poke-ball"),
@@ -381,6 +376,7 @@ ITEM_EMOJIS = {
 	"tm50": "<:tmfire:1424516746383196182>",
 
 }
+
 
 
 
