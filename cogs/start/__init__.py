922ed8baf27329e977eddf767162f82d5c581aff
Ueslei Paim <uesleibros@gmail.com>
2025-10-18 09:21:44 -0300

    Update __init__.py

--- cogs/start/__init__.py
+++ cogs/start/__init__.py
@@ -1,6 +1,6 @@
 import discord
 import helpers.checks as checks
-from cogs.start.views import StarterView
+from cogs.start.views import AccountCreationView
 from discord.ext import commands
 
 class Start(commands.Cog, name="Começar"):
@@ -12,9 +12,9 @@ class Start(commands.Cog, name="Começar"):
 	@checks.require_no_account()
 	async def start_command(self, ctx: commands.Context):
 		user_id: str = str(ctx.author.id)
-		view = StarterView(user_id)
+		view = AccountCreationView(user_id)
 
-		await ctx.message.reply(f"{ctx.author.mention} Escolha seu inicial!\n-# Escolha sabiamente, ele acompanhará você até largar por outro...", view=view)
+		await ctx.reply(f"Vamos criar a sua conta!", view=view)
 
 async def setup(bot: commands.Bot):
 	await bot.add_cog(Start(bot))
