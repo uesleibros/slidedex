8385a69aee86091046a2a1a01015c8c927c74b0c
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2025-10-18 21:52:48 -0300

    update: adicionado comando 'profile' e pseudo sistema de locomoção.

--- cogs/profile/__init__.py
+++ cogs/profile/__init__.py
@@ -9,30 +9,30 @@ from sdk.toolkit import Toolkit
 class Profile(commands.Cog, name="Perfil"):
 	def __init__(self, bot: commands.Bot) -> None:
 		self.bot = bot
-		self.tk: Toolkit = Toolkit()
+		self.tk = Toolkit()
 
 	@commands.command(name="profile", aliases=["pf"])
 	@checks.require_account()
 	async def profile_command(self, ctx: commands.Context) -> None:
 		await ctx.defer()
 
-		uid: str = str(ctx.author.id)
-		user: dict = self.tk.users.get(uid)
-		user_party: dict = self.tk.pokemon.get_party(uid)
-		party_sprites: List[bytes] = []
-
-		for poke in user_party:
-			party_sprites.append(self.tk.api.get_pokemon_sprite(poke)[0])
-
-		background = preloaded_textures["profile"]
-		buf = await compose_profile_async(party_sprites, background)
-		img_file = discord.File(buf, filename="profile.png")
-		location_file = discord.File(f"resources/locations/kanto/{user['location']}.png", "location.png")
-		pokemon_count: int = self.tk.pokemon.count_all(uid)
-
-		view: discord.ui.LayoutView = ProfileLayoutView(user, pokemon_count)
-
-		await ctx.reply(view=view, files=[img_file, location_file])
+		uid = str(ctx.author.id)
+		tk = self.tk
+		
+		user = tk.users.get(uid)
+		party_sprites = [tk.api.get_pokemon_sprite(p)[0] for p in tk.pokemon.get_party(uid)]
+		
+		buf = await compose_profile_async(party_sprites, preloaded_textures["profile"])
+		
+		files = [
+			discord.File(buf, filename="profile.png"),
+			discord.File(f"resources/locations/kanto/{user['location']}.png", "location.png")
+		]
+		
+		await ctx.reply(
+			view=ProfileLayoutView(user, tk.pokemon.count_all(uid)),
+			files=files
+		)
 
 async def setup(bot: commands.Bot) -> None:
 	await bot.add_cog(Profile(bot))
